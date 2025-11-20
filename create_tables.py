4ee0983204de0a8ac8a72962584010e31e15cbbe
Vandanaa M <vandana69430@gmail.com>
2025-11-20 21:14:22 +0530

    Added create_tables.py for Render DB creation

--- create_tables.py
+++ create_tables.py
@@ -3,7 +3,7 @@ from app import app
 from extensions import db 
 import models
 
-if __name__="__main__":
+if __name__=="__main__":
     with app.app_context():
         db.create_all()
         print("All tables created successfully.")
